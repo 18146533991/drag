8d70f6bb329618bcc977604d40317a3c355a266d
18146533991 <zxs_6666@163.com>
2015-12-28 18:18:47 +0800

    change

--- drag.js
+++ drag.js
@@ -1,9 +1,13 @@
 function Drag(){
-	this.id=null;
+	this.obj=null;
+	this.disX=null;
+	this.disY=null;
 }
 Drag.prototype.init=function(id){
 	this.obj=document.getElementById(id);
 	this.obj.onmousedown=function(ev){
 		ev=ev||event;
+		this.disX=ev.clientX-this.obj.offsetLeft;
+		this.disX=ev.clientY-this.obj.offsetTop;
 	}
 }
